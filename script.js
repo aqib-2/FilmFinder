a1819ca4d4d82ca60a2cf771051ea7305e84d2a8
Aqib <124564203+aqib-2@users.noreply.github.com>
2023-05-16 15:33:48 +0530

    Update script.js
    Removed genre tags

--- script.js
+++ script.js
@@ -7,25 +7,6 @@ const searchURL = URL + "/search/movie?" + apiKey + "&query=";
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-const tagEle = document.getElementById('tags');
-
-const genres=[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}];
-
-setGenre();
-//function to display genres
-function setGenre(){
-   tagEle.innerHTML='';
-   genres.forEach(genre => {
-     const t=document.createElement('div');
-     t.classList.add('tag');
-     t.id=genre.id;
-     t.innerText=genre.name;
-     t.addEventListener('click', () => {
-      
-     })
-     tagEle.appendChild(t);
-   })
-}
 
 getMovies(apiURL);//Invoking the function to display data on html page
 
